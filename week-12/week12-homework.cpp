7b571dfafeea4de74c813a6259c80ec0a4a92c98
Sergi Castells <sergicastells9@gmail.com>
2021-11-22 21:36:25 -0500

    Finished week 12 work.

--- week-12/week12-homework.cpp
+++ week-12/week12-homework.cpp
@@ -53,7 +53,7 @@ void week12_homework() {
 	// Set up various arrays for later
 	Double_t sigma = 2;
 	int min[arraySize], max[arraySize];
-	Double_t mean[arraySize], x_error[arraySize];
+	Double_t mean[arraySize], x_error[arraySize], zero[arraySize];
 	Double_t energy[arraySize] = {121.78, 244.7, 344.28, 367.79, 411.12, 443.97, 688.67, 778.9,
 							879.38, 964.08, 1085.87, 1089.74, 1112.07, 1212.95, 1299.14, 1408.01};
 	
@@ -65,16 +65,17 @@ void week12_homework() {
 		TF1* f1 = new TF1("f1", "gaus", min[i], max[i]);
 		h1->Fit(f1, "QR0");
 		mean[i] = f1->GetParameter(1);
-		x_error[i] = 1000*(f1->GetParError(1)); // errors too small to be visible
+		x_error[i] = (f1->GetParError(1));
+		zero[i] = 0.01;
 	}
 	
 	// TGraphError setup
-	TGraphErrors* graph = new TGraphErrors(arraySize, mean, energy, x_error, 0);
+	TGraphErrors* graph = new TGraphErrors(arraySize, mean, energy, x_error, zero);
 	
 	// Set up graph
 	canvas->cd(2);
 	graph->SetTitle("Energy vs Channel");
-	graph->GetYaxis()->SetTitle("Energy (GeV)");
+	graph->GetYaxis()->SetTitle("Energy (keV)");
 	graph->GetYaxis()->CenterTitle();
 	graph->GetYaxis()->SetTitleSize(0.05);
 	graph->GetXaxis()->SetTitle("Channel");
@@ -85,17 +86,22 @@ void week12_homework() {
 	// Draw and fit graph
 	graph->Draw("A*");
 	TF1* f2 = new TF1("f2", "pol2", 0, 8192);
-	graph->Fit(f2, "RQ");
+	graph->Fit(f2);
+	f2->SetNpx(1000);
+	f2->SetLineStyle(kDashed);
 	
 	
 	// Set up residuals and y_error array
 	Double_t residuals[arraySize];
 	Double_t y_error[arraySize];
 	
-	// Fill residuals array
+	// Fill residuals and y_errors array
 	for(int i = 0; i < arraySize; i++) {
 		residuals[i] = energy[i] - f2->Eval(mean[i]);
-		y_error[i] = f2->GetParError(2); // get actual y-error here
+		
+		// propagate quadradtic and energy error
+		y_error[i] = std::sqrt(std::pow(f2->GetParError(0),2) + std::pow(mean[i]*f2->GetParError(1),2)
+							+ std::pow(std::pow(mean[i],2) * f2->GetParError(2),2) + std::pow(0.01,2));
 	}
 	
 	// TGraphError setup
@@ -104,13 +110,12 @@ void week12_homework() {
 	// Set up graph
 	canvas->cd(3);
 	res_graph->SetTitle("Residuals");
-	res_graph->GetYaxis()->SetTitle("E_{#gamma} - E_{fit}(GeV)");
+	res_graph->GetYaxis()->SetTitle("E_{#gamma} - E_{fit}(keV)");
 	res_graph->GetYaxis()->CenterTitle();
 	res_graph->GetYaxis()->SetTitleSize(0.05);
 	res_graph->GetXaxis()->SetTitle("Channel");
 	res_graph->GetXaxis()->CenterTitle();
 	res_graph->GetXaxis()->SetTitleSize(0.05);
-	res_graph->SetMinimum(0);
 	
 	// Draw graph
 	res_graph->Draw("A*");
